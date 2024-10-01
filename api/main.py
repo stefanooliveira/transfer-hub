7b877e241408ae5edfc1d0f0d4b847ec52405863
stefanooliveira <stefanocamposdeoliveira@gmail.com>
2024-09-30 21:46:54 -0300

    Modularize route to handler functions; Port changed; Add handler to events

--- api/main.py
+++ api/main.py
@@ -7,4 +7,4 @@ app = FastAPI()
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5049)
+    uvicorn.run(app, host="127.0.0.1", port=3000)
--- api/routes.py
+++ api/routes.py
@@ -1,21 +1,30 @@
-from fastapi import APIRouter, HTTPException, status, Request
-from fastapi.responses import JSONResponse
+# routes.py
+from fastapi import APIRouter, Request
+from fastapi.responses import JSONResponse, PlainTextResponse
+from api.handler import handle_reset, handle_get_balance, handle_deposit, handle_withdraw, handle_transfer
 
 router = APIRouter()
-
-state = {}
 
 @router.get("/")
 async def root():
     return {"message": "Welcome to transfer-hub"}
 
-@router.post("/reset", status_code=status.HTTP_200_OK)
+@router.post("/reset")
 def reset():
-    state.clear()
-    return JSONResponse(content={"status": "reset"}, status_code=status.HTTP_200_OK)
+    return handle_reset()
 
-@router.get("/balance", status_code=status.HTTP_200_OK)
+@router.get("/balance")
 def get_balance(account_id: str):
-    if account_id not in state:
-        raise HTTPException(status_code=404, detail="Account not found")
-    return JSONResponse(content={"balance": state[account_id]}, status_code=status.HTTP_200_OK)
+    return handle_get_balance(account_id)
+
+@router.post("/event")
+async def handle_event(request: Request):
+    event = await request.json()
+    if event['type'] == "deposit":
+        return handle_deposit(event['destination'], event['amount'])
+    elif event['type'] == "withdraw":
+        return handle_withdraw(event['origin'], event['amount'])
+    elif event['type'] == "transfer":
+        return handle_transfer(event['origin'], event['destination'], event['amount'])
+    else:
+        return PlainTextResponse(content="Invalid event type", status_code=400)
